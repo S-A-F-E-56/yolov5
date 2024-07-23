e11ecc28692c6feed015e7a2305d2296b4d27f13
FzPrograms <faizalfarisi67@gmail.com>
2024-07-24 00:02:13 +0700

    Saving detected image to fixed folder

--- detect.py
+++ detect.py
@@ -265,10 +265,10 @@ def run(
                     label = names[c] if hide_conf else f"{names[c]}"
                     confidence = float(conf)
                     confidence_str = f"{confidence:.2f}"
-
+                    '''
                     if save_csv:
                         write_to_csv(p.name, label, confidence_str)
-                    '''
+                    
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
